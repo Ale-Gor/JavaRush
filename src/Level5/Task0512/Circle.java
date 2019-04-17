15baaa106f7aee829d4f429f40650f2c3236ac78
Ale-gor <ale-gor@mail.ru>
2019-04-17 18:49:27 +0200

    Task0512 added

---
+++ src/Level5/Task0512/Circle.java
@@ -0,0 +1,33 @@
+package Level5.Task0512;
+
+public class Circle {
+
+  int centerX;
+  int centerY;
+  int radius;
+  int width;
+  String color = null;
+
+  public void initialize(int centerX, int centerY, int radius, int width, String color) {
+    this.centerX = centerX;
+    this.centerY = centerY;
+    this.radius = radius;
+    this.width = width;
+    this.color = color;
+  }
+
+  public void initialize(int centerX, int centerY, int radius, int width) {
+    this.centerX = centerX;
+    this.centerY = centerY;
+    this.radius = radius;
+    this.width = width;
+    this.color = color;
+  }
+
+
+
+  public static void main(String[] args) {
+
+  }
+
+}
