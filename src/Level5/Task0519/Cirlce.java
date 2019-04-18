f06120d03ee9b4e9dee525b446c85e998d0c8af6
Ale-gor <ale-gor@mail.ru>
2019-04-18 17:33:20 +0200

    Task0524 added

---
+++ src/Level5/Task0519/Cirlce.java
@@ -0,0 +1,36 @@
+package Level5.Task0519;
+
+public class Cirlce {
+
+  int centerX;
+  int centerY;
+  int raduis;
+  int wigth;
+  String color;
+
+  public Cirlce(int centerX, int centerY, int raduis) {
+    this.centerX = centerX;
+    this.centerY = centerY;
+    this.raduis = raduis;
+  }
+
+  public Cirlce(int centerX, int centerY, int raduis, int wigth) {
+    this.centerX = centerX;
+    this.centerY = centerY;
+    this.raduis = raduis;
+    this.wigth = wigth;
+  }
+
+  public Cirlce(int centerX, int centerY, int raduis, int wigth, String color) {
+    this.centerX = centerX;
+    this.centerY = centerY;
+    this.raduis = raduis;
+    this.wigth = wigth;
+    this.color = color;
+  }
+
+  public static void main(String[] args) {
+
+  }
+
+}
