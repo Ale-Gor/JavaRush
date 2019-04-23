4ef9eb67265575192c3e3b807aa753100d83e527
Ale-gor <ale-gor@mail.ru>
2019-04-23 16:16:55 +0200

    Task0531 added

---
+++ src/Level5/Task0530/Solution.java
@@ -0,0 +1,16 @@
+package Level5.Task0530;
+
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Solution {
+  public static void main(String[] args) throws Exception {
+    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    int a = Integer.parseInt(reader.readLine());
+    int b = Integer.parseInt(reader.readLine());
+
+    int sum = a + b;
+    System.out.println("Sum = " + sum);
+  }
+}
