ae3644e5a21e8da40588a15e3d0fef1bad3108ca
Ale-gor <ale-gor@mail.ru>
2019-04-27 13:37:19 +0200

    Task0610 added

---
+++ src/Level6/Task0610/ConsoleReader.java
@@ -0,0 +1,45 @@
+package Level6.Task0610;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+/*
+Класс ConsoleReader
+*/
+
+public class ConsoleReader {
+
+
+
+  public static String readString() throws Exception {
+    Scanner sc = new Scanner(System.in);
+    return sc.nextLine();
+
+  }
+
+  public static int readInt() throws Exception {
+    //напишите тут ваш код
+    Scanner sc = new Scanner(System.in);
+    return sc.nextInt();
+
+  }
+
+  public static double readDouble() throws Exception {
+    //напишите тут ваш код
+    Scanner sc = new Scanner(System.in);
+    return sc.nextDouble();
+
+  }
+
+  public static boolean readBoolean() throws Exception {
+    //напишите тут ваш код
+    Scanner sc = new Scanner(System.in);
+    return sc.nextBoolean();
+
+  }
+
+  public static void main(String[] args) {
+
+  }
+}
