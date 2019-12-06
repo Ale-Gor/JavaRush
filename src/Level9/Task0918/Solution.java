9ca09abef581ac8c34a4254af84e4a736409b721
Ale-gor <ale-gor@mail.ru>
2019-12-06 13:51:33 +0100

    Task0918 added

---
+++ src/Level9/Task0918/Solution.java
@@ -0,0 +1,24 @@
+package Level9.Task0918;
+
+
+/*
+Все свои, даже исключения
+*/
+
+public class Solution {
+  public static void main(String[] args) {
+  }
+
+  static class MyException {
+  }
+
+  static class MyException2 {
+  }
+
+  static class MyException3 {
+  }
+
+  static class MyException4 {
+  }
+}
+
